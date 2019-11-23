8ce7d5fa2e0ee7f1f5a7fc0e30ac6d52b2c09160
Alex Nichol <unixpickle@gmail.com>
2019-11-23 14:06:39 -0500

    polynomial for log sigmoid

--- math_util.go
+++ math_util.go
@@ -66,3 +66,62 @@ func gradientMean(ts []*TimestepSample) []float32 {
 	}
 	return sum
 }
+
+type polynomial []float32
+
+// newPolynomialLogSigmoid approximates the log of the
+// sigmoid function as a high-order polynomial.
+// The resulting polynomial is p(a) ~= log(sigmoid(x+a)).
+// It is very accurate when abs(a) < 1.
+func newPolynomialLogSigmoid(x float32) polynomial {
+	// Create a short Taylor series.
+	exp := float32(math.Exp(float64(x)))
+	invExp := float32(math.Exp(float64(-x)))
+	if math.IsInf(float64(exp), 1) {
+		exp = float32(math.Exp(12))
+	} else if math.IsInf(float64(invExp), 1) {
+		invExp = float32(math.Exp(12))
+	}
+
+	logValue := x
+	if x > -12 {
+		logValue = float32(math.Log(float64(1 / (1 + invExp))))
+	}
+
+	exp2 := exp * exp
+	exp3 := exp2 * exp
+	exp4 := exp2 * exp2
+	exp5 := exp3 * exp2
+	exp6 := exp3 * exp3
+
+	expP := exp + 1
+	expP2 := expP * expP
+	expP3 := expP2 * expP
+	expP4 := expP2 * expP2
+	expP5 := expP4 * expP
+	expP6 := expP3 * expP3
+	expP7 := expP4 * expP3
+	expP8 := expP4 * expP4
+
+	return polynomial{
+		logValue,
+		1 / (exp + 1),
+		-1.0 / 2.0 * exp / expP2,
+		1.0 / 6.0 * exp * (exp - 1) / expP3,
+		-1.0 / 24.0 * exp * (-4*exp + exp2 + 1) / expP4,
+		1.0 / 120.0 * exp * (11*exp - 11*exp2 + exp3 - 1) / expP5,
+		-1.0 / 720.0 * exp * (-26*exp + 66*exp2 - 26*exp3 + exp4 + 1) / expP6,
+		1.0 / 5040.0 * exp * (57*exp - 302*exp2 + 302*exp3 - 57*exp4 + exp5 - 1) / expP7,
+		-1.0 / 40320.0 * exp * (-120*exp + 1191*exp2 - 2416*exp3 + 1191*exp4 - 120*exp5 + exp6 + 1) / expP8,
+	}
+}
+
+func (p polynomial) Evaluate(x float32) float32 {
+	coeff := float32(1)
+	res := float32(0)
+	for _, c := range p {
+		res += c * coeff
+		coeff *= x
+	}
+	return res
+}
---
+++ math_util_test.go
@@ -0,0 +1,30 @@
+package seqtree
+
+import (
+	"math"
+	"testing"
+)
+
+func TestPolynomialLogSigmoid(t *testing.T) {
+	centers := []float32{-30, -20, -10, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 10, 20, 30}
+	for _, center := range centers {
+		p := newPolynomialLogSigmoid(center)
+		for x := float32(-1.0); x <= 1.0; x += 0.1 {
+			actual := p.Evaluate(x)
+			expected := exactLogSigmoid(center + x)
+			if math.Abs(float64(actual-expected)) > 1e-4 {
+				t.Errorf("log(sigmoid(%f+%f)) should be %f but got %f",
+					center, x, expected, actual)
+			}
+		}
+	}
+}
+
+func exactLogSigmoid(x float32) float32 {
+	if x < -20 {
+		return x
+	} else if x > 20 {
+		return float32(math.Exp(float64(-x)))
+	}
+	return float32(math.Log(1 / (1 + math.Exp(float64(-x)))))
+}
