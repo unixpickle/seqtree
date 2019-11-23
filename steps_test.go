cde5eb236a11c2b0563edb9a326797cf3fbc7dcb
Alex Nichol <unixpickle@gmail.com>
2019-11-23 14:08:01 -0500

    move some training code to separate steps.go

--- steps_test.go
+++ steps_test.go
@@ -3,7 +3,6 @@ package seqtree
 import (
 	"fmt"
 	"math"
-	"math/rand"
 	"runtime"
 	"testing"
 )
@@ -80,47 +79,4 @@ func bruteForceOptimalStep(ts []*TimestepSample, t *Tree, stepsLeft int, min, ma
 			minStep+(max-min)/100)
 	}
 	return minStep
-}
-
-func generateTestModel(baseFeatures int) *Model {
-	m := &Model{BaseFeatures: baseFeatures}
-	for i := 0; i < 4; i++ {
-		b := &Builder{
-			Depth:           3,
-			MinSplitSamples: 10,
-			Horizons:        []int{0, 1, 2},
-		}
-		t := b.Build(AllTimesteps(generateTestSequences(m)...))
-		m.Add(t, 0.1)
-	}
-	return m
-}
-
-func generateTestSequences(m *Model) []Sequence {
-	var res []Sequence
-	for i := 0; i < 15; i++ {
-		seq := make([]int, 20)
-		for i := range seq {
-			seq[i] = rand.Intn(m.BaseFeatures)
-		}
-		res = append(res, MakeOneHotSequence(seq, m.BaseFeatures, m.NumFeatures()))
-	}
-	m.EvaluateAll(res)
-	PropagateLosses(res)
-	return res
-}
-
-func BenchmarkBuildTreeDense(b *testing.B) {
-	seqInts := make([]int, 768)
-	for i := range seqInts {
-		seqInts[i] = rand.Intn(2)
-	}
-	m := &Model{BaseFeatures: 2}
-	seq := MakeOneHotSequence(seqInts, 2, m.NumFeatures())
-	PropagateLosses([]Sequence{seq})
-	builder := Builder{Depth: 3, Horizons: []int{0, 1, 2, 28, 29, 30, 56, 57, 58}}
-	b.ResetTimer()
-	for i := 0; i <= b.N; i++ {
-		builder.Build(AllTimesteps(seq))
-	}
 }
