67e3c8b1d3c6fc97c558975805aea21968c9b231
Alex Nichol <unixpickle@gmail.com>
2019-11-29 15:56:37 -0500

    fix error

--- enc_mnist/main.go
+++ enc_mnist/main.go
@@ -28,7 +28,7 @@ func main() {
 	seqModel.Load("sequence_model.json")
 	log.Println("Training sequence model...")
 	seqs := encoder.EncodeBatch(dataset, len(dataset.Samples))
-	testSeqs := encoder.EncodeBatch(dataset, len(dataset.Samples))
+	testSeqs := encoder.EncodeBatch(testDataset, len(dataset.Samples))
 	for {
 		testLoss := seqModel.MeanLoss(testSeqs)
 		loss, delta := seqModel.AddTree(seqs)
